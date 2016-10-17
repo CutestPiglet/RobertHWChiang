e8b1d69b75245a1787d05cef7c73f1af8a0de7f5
RobertH-W-Chiang <johnmay0629@gmail.com>
2016-10-17 07:40:04 +0000

    pass Django version from backend

--- hwchiang/header/views.py
+++ hwchiang/header/views.py
@@ -1,5 +1,8 @@
+import django
 from django.shortcuts import render
 
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request,
+                  'about.html',
+                  {'django_version': django.get_version()})
