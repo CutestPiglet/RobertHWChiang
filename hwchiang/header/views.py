ee394de522a0cc47fc79c8818d44b1b5c410b237
RobertH-W-Chiang <johnmay0629@gmail.com>
2016-10-21 01:14:50 +0000

    move box contents from about.html to views.py

--- hwchiang/header/views.py
+++ hwchiang/header/views.py
@@ -3,9 +3,60 @@ from django.shortcuts import render
 
 
 def about(request):
-    return render(request,
-                  'about.html',
-                  {'django_version': django.get_version()})
+    boxes = []
+
+    # < about this site >
+    contents = []
+    contents.append({
+        'p_text': 'Wrote by Django {}'.format(django.get_version()),
+    })
+    contents.append({
+        'p_text': 'Host on',
+        'a_text': 'Heroku',
+        'a_href': 'https://www.heroku.com/'
+    })
+    contents.append({
+        'p_text': 'Developed on Ubuntu 14.04'
+    })
+    contents.append({
+        'p_text': 'Find source code on',
+        'a_text': 'my GitHub repository',
+        'a_href': 'https://github.com/RobertH-W-Chiang/RobertHWChiang'
+    })
+    boxes.append({
+        'heading_img_url': 'img/about/site.jpg',
+        'title': 'About This Site',
+        'contents': contents
+    })
+
+    # < about me >
+    contents = []
+    contents.append({
+        'p_text': 'Email:',
+        'a_text': 'johnmay0629@gmail.com',
+        'a_href': 'mailto:johnmay0629@gmail.com'
+    })
+    contents.append({
+        'p_text': 'Flickr:',
+        'a_text': 'https://www.flickr.com/photos/johnmay0629',
+        'a_href': 'https://www.flickr.com/photos/johnmay0629'
+    })
+    contents.append({
+        'p_text': 'Youtube:',
+        'a_text': 'https://www.youtube.com/user/johnmay0629',
+        'a_href': 'https://www.youtube.com/user/johnmay0629/videos'
+    })
+    contents.append({
+        'p_text': 'Facebook:',
+        'a_text': 'https://www.facebook.com/johnmay0629',
+        'a_href': 'https://www.facebook.com/johnmay0629'
+    })
+    boxes.append({
+        'heading_img_url': 'img/about/me.jpg',
+        'title': 'About Me',
+        'contents': contents
+    })
+    return render(request, 'about.html', {'boxes': boxes})
 
 
 def resume(request):
